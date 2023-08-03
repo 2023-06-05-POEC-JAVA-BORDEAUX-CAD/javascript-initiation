01aaa9e406eeac1633e50d613525fafb4799d7bb
Chris Lisangola <jochri3@gmail.com>
2023-08-03 16:28:09 +0300

    6.Tableaux en JS

---
+++ 4.tableaux/tb.java
@@ -0,0 +1,12 @@
+double[] notes={12,4.5,9,11.3,14};//xo1234
+
+notes[0]=19
+System.out.println(notes[0])//19
+
+double[] notes2=notes;//xo1234
+notes2[0]=33
+System.out.println(notes[0])//33
+
+
+final double[] nombres={1,2,3,4,5,6}//x0123
+nombres[0]=100;
