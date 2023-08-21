0591507237a8d8d539dbb711330f0133bbb54ebe
Christian Lisangola <jochri3@gmail.com>
2023-08-21 13:21:29 +0300

    9.Object litterals in JS

---
+++ 7.poo/app3.js
@@ -0,0 +1,12 @@
+const article = {
+  category: "Computers",
+  name: "Macbook Pro 16 M1 Max",
+  price: 3500,
+  stk: 700,
+  description: "Very good laptop",
+};
+
+console.log(article.category);
+console.log(article.name);
+console.log(article.price);
+console.log(article.description);
