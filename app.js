1171664ba215554e028ae3c0da61b142a285b743
Christian Lisangola <jochri3@gmail.com>
2023-08-21 16:39:32 +0300

    13.Event listeners

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const frontend = document.querySelectorAll(".frontend");
 
-// background-color -> backgroundColor
-// font-size -> fontSize
-// frontend[0].style.backgroundColor = "red";
-// frontend[0].style.color = "white";
-// frontend[0].style.fontWeight = "bold";
-// frontend[0].style.fontSize = "25px";
-// frontend[0].style.padding = "10px";
-
-for (let techno of frontend) {
-  techno.style.backgroundColor = "red";
-  techno.style.color = "white";
-  techno.style.fontWeight = "bold";
-  techno.style.fontSize = "25px";
-  techno.style.padding = "10px";
-  techno.style.border = "2px solid yellow";
+for (let front of frontend) {
+  // front.classList.add("front");
+  // front.className = "front";
+  // Rappel ou Callback
+  front.addEventListener("click", function () {
+    // console.log(`Contenu : ${front.innerText}`);
+    console.log(`Contenu : ${this.innerText}`);
+    // this.classList.add("front"); Ajouter une classe
+    this.classList.toggle("front");
+  });
 }
